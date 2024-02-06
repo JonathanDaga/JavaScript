8846c674ad5230562ce41185825682ba543c8246
Jonathan Daga <jonathandaga14@gmail.com>
2024-02-06 01:04:11 -0300

    agrego productos

--- main.js
+++ main.js
@@ -1,49 +1,126 @@
-let carrito = [];
+const modal = new bootstrap.Modal('#modalCarrito', {});
+const btnModalCarrito = document.querySelector('#btnModalCarrito');
+const cartCount = document.querySelector('#cartCount');
+const cartSum = document.querySelector('#cartSum');
+const inputSearch = document.querySelector('#inputSearch');
+const listProducts = document.querySelector('#listProducts');
+const selectCategory = document.querySelector('#selectCategory');
+const modalListProducts = document.querySelector('#modalListProducts');
+const btnClose = document.querySelector('#btnClose');
+const btnSave = document.querySelector('#btnSave');
+const btnOrder = document.querySelector('#btnOrder');
 
-function agregarProductoAlCarrito() {
-    let producto = prompt('¿Qué producto estás buscando?');
-    carrito.push(producto);
-    actualizarCarritoEnDOM();
-}
+const listCart = JSON.parse(localStorage.getItem('cart')) || [];
+const cart = new Cart(listCart);
 
-function actualizarCarritoEnDOM() {
-    const carritoContainer = document.getElementById('carrito-container');
-    carritoContainer.innerHTML = ''; // Limpiamos el contenido anterior del carrito
+cartCount.innerText = cart.getCount();
 
-    carrito.forEach((producto) => {
-        const productoElement = document.createElement('p');
-        productoElement.textContent = producto;
-        carritoContainer.appendChild(productoElement);
+
+btnModalCarrito.addEventListener('click', function () {
+    const list = cart.getProducts();
+    cartSum.innerText = cart.getSum();
+
+    redenCart(list);
+
+    modal.show();
+})
+
+btnSave.addEventListener('click', () => {
+    console.log('Inicio')
+    setTimeout(() => {
+
+
+        Swal.fire({
+            title: "Carrito de Compras",
+            text: "Compra finalizada",
+            icon: "success"
+        });
+
+    }, 3000)
+
+    modal.hide();
+
+    localStorage.removeItem('cart');
+})
+
+btnClose.addEventListener('click', () => {
+    modal.hide();
+})
+
+inputSearch.addEventListener('input', (event) => {
+    const search = event.target.value;
+    const newList = products.filter((product) => product.name.toLowerCase().includes(search.toLowerCase()));
+    renderProducts(newList);
+})
+
+btnOrder.addEventListener('click', () => {
+    products.sort((a, b) => {
+        if (a.price < b.price) {
+            return -1
+        }
+        if (a.price > b.price) {
+            return 1
+        }
+
+        return 0
+    })
+
+    renderProducts(products)
+    btnOrder.setAttribute('disabled', true)
+})
+
+const renderProducts = (list) => {
+    listProducts.innerHTML = '';
+    list.forEach(product => {
+        listProducts.innerHTML =
+            `<div class="col-sm-4 col-md-3">
+                <div class="card p-2">
+                    <h4 class="text-center">${product.name} </h4>
+                    <img class="img-fluid" src="${product.img}" alt="${product.name}">
+                    <h3 class="text-center">$${product.price} </h3>
+                    <button id="${product.id} " type="button" class="btn btn-primary btnAddCart">
+                        <i class="fa-solid fa-cart-plus"></i> Agregar
+                    </button>
+                </div>
+            </div>`;
+    });
+
+    const btns = document.querySelectorAll('.btnAddCart');
+    btns.forEach(btn => {
+        btn.addEventListener('click', addToCart);
     });
 }
 
-function totalCompra() {
-    let total = 0;
-    return total;
+const addToCart = (e) => {
+    const id = e.target.id;
+    const product = products.find(item => item.id == id);
+    console.table(product);
+    cart.addToCart(product);
+    cartCount.innerText = cart.getCount();
+
+    Toastify({
+        close: true,
+        text: "Producto agregado al Carrito",
+        gravity: 'bottom',
+        duration: 3000,
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+    }).showToast();
+
 }
 
-function agregarIVA(total) {
-    const IVA = 0.21;
-    const totalConIVA = total + (total * IVA);
-    return totalConIVA;
-}
+const redenCart = (list) => {
+    modalListProducts.innerHTML = '';
+    list.forEach(product => {
+        modalListProducts.innerHTML =
+            `<tr>
+                <td> ${product.name} </td>
+                <td> ${product.units}</td>
+                <td>$${product.price}</td>
+                <td>$${product.price * product.units}</td>
 
-function ProductosDisponibles() {
-    let productos = ['Camiseta seleccion argentina qatar 2022', 'Camiseta seleccion argentina finalissima', 'Camiseta seleccion argentina copa america 2021'];
-    for (let i = 0; i < productos.length; i++) {
-        console.log(productos[i]);
-    }
-}
-
-function aplicarOperacionACarrito(operacion) {
-    carrito.forEach((producto) => {
-        operacion(producto);
+            </tr>`
     });
 }
-
-function imprimirProducto(producto) {
-    console.log("Producto en el carrito: " + producto);
-}
-
-const agregarProductoBtn = document.getElementById('agregar-producto-btn');
-agregarProductoBtn.addEventListener('click', agregarProductoAlCarrito);
+renderProducts(products);
---
+++ productos.js
@@ -0,0 +1,6 @@
+const products = [
+    { id: 1, name: 'Camiseta Qatar 2022',  price: 65000, category: 'camisetas'},
+    { id: 2, name: 'Camiseta Copa America 2021', price: 55000, category: 'camisetas'},
+    { id: 3, name: 'Camiseta Finalissima', price: 45000, category: 'camisetas'},
+    { id: 4, name: 'Short Afa', price: 35000, category: 'short'}
+]
